512ec55850ec642671e2f4e18312590ae61b4524
adarsh <adarshmaurya9415@gmail.com>
2023-10-02 18:22:25 +0530

    initial commit

--- home/views.py
+++ home/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+
+from django.http import JsonResponse
+
+# from .models import Recipe
 
 # Create your views here.
+
+def index(request):
+    return render(request, "index.html") # you can also give variables ,
