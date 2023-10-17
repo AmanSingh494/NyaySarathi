0b0fa79738749267e8e7f8a6d3de7558273e86b7
adarsh <adarshmaurya9415@gmail.com>
2023-10-17 14:12:49 +0530

    modified vidura for fetching and communicating with our api requests

--- static/js/vidura.js
+++ static/js/vidura.js
@@ -1,1 +1,51 @@
-console.log("I am working fine! ");
+console.log("I am working fine!");
+const AUTHOR = "Adarsh Maurya";
+console.log(`Author ${AUTHOR}`);
+
+// By chatgpt - needs modifications and might contain errors 
+
+document.addEventListener("DOMContentLoaded", function() {
+    const chatForm = document.getElementById("chat-form");
+    const userMessage = document.getElementById("message");
+    const mainChat = document.getElementById("main-chbot");
+
+    // Function to add a user message to the chat interface
+    function addUserMessage(message) {
+        const userMessageDiv = document.createElement("div");
+        userMessageDiv.className = "user-message";
+        userMessageDiv.innerHTML = `<p>${message}</p>`;
+        mainChat.appendChild(userMessageDiv);
+    }
+
+    // Function to add a bot message to the chat interface
+    function addBotMessage(message) {
+        const botMessageDiv = document.createElement("div");
+        botMessageDiv.className = "bot-message";
+        botMessageDiv.innerHTML = `<p>${message}</p>`;
+        mainChat.appendChild(botMessageDiv);
+    }
+
+    // Handle form submission
+    chatForm.addEventListener("submit", function(event) {
+        event.preventDefault();
+        const userText = userMessage.value;
+
+        // Add the user's message to the chat interface
+        addUserMessage(userText);
+
+        // Send the user's message to the server for processing
+        fetch("/vidura", {
+            method: "POST",
+            body: JSON.stringify({ message: userText }),
+            headers: { "Content-Type": "application/json" },
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                const viduraResponse = data.message;
+                addBotMessage(viduraResponse);
+            });
+
+        // Clear the user input field
+        userMessage.value = "";
+    });
+});
