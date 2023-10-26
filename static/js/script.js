87fa38b502905a017276b4d50b48643dc8cab00c
AmanSingh494 <amanforwork1@gmail.com>
2023-10-26 20:03:07 +0530

    changed logo, added components e.t.c.

--- static/js/script.js
+++ static/js/script.js
@@ -1,32 +1,38 @@
-console.log("I am working!");
+console.log('I am working!')
 
+const author = 'Adarsh Maurya!'
 
-const author = "Adarsh Maurya!";
+console.log(`Developer ${author}`)
 
-console.log(`Developer ${author}`);
+let menuBtnCond = true
+let checkLabel = document.getElementById('check-label')
+const faqs = document.querySelectorAll('.faqs')
 
+const menuBtnChanger = function () {
+  if (menuBtnCond) {
+    checkLabel.innerHTML = `<i class='bx bx-x'></i>`
+    menuBtnCond = false
+  } else {
+    checkLabel.innerHTML = `<i class='bx bx-menu'></i>`
 
-
-
-
-
-  let menuBtnCond = true;
-  let checkLabel = document.getElementById("check-label");
-  
-  const menuBtnChanger = function(){
-  
-      if (menuBtnCond){
-          checkLabel.innerHTML = `<i class='bx bx-x'></i>`;
-          menuBtnCond = false;
-      }
-  
-      else{
-          checkLabel.innerHTML = `<i class='bx bx-menu'></i>`;
-  
-          menuBtnCond = true;
-      }
-  
+    menuBtnCond = true
   }
-  
-  let menuBtn = document.getElementById("check");
-  menuBtn.addEventListener("click", menuBtnChanger);
+}
+// button on click animation
+const btnAnimation = () => {}
+faqs.forEach((faq) => {
+  const faqsBtn = faq.querySelector('.btn-rotate')
+  const faqsInfo = faq.querySelector('.faqs-info')
+  faqsBtn.addEventListener('click', (item) => {
+    if (item !== faq) {
+      faqsInfo.classList.remove('show')
+    }
+    faqsBtn.classList.toggle('rotate')
+    faqsInfo.classList.toggle('show')
+  })
+})
+console.log(faqsDivs)
+// faqsBtn.addEventListener('click', btnAnimation)
+
+let menuBtn = document.getElementById('check')
+menuBtn.addEventListener('click', menuBtnChanger)
